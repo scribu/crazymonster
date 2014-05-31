d4b4807ae36885157ba8df81d12c5e765d62bf86
scribu <mail@scribu.net>
2014-05-31 22:51:25 +0300

    stabalization all the things

--- leap-sphero.js
+++ leap-sphero.js
@@ -13,7 +13,7 @@ var C = spheron.toolbelt.COLORS;
 
 var flipMacroId = 101;
 var flip = spheron.macro(flipMacroId);
-flip.append(macro.setStabilization(false));
+flip.append(macro.setStabalization(false));
 flip.append(macro.setRawMotorValues(0x01, 255, 0x01, 253, 255));
 flip.append(macro.setRGB(C.GREEN, 60));
 flip.append(macro.goto(flip.id()));
