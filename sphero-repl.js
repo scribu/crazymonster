fed8d5009933e69977d2ec396109e54d37ab20c8
scribu <mail@scribu.net>
2014-05-31 20:17:53 +0300

    add sphero-repl.js

---
+++ sphero-repl.js
@@ -0,0 +1,44 @@
+var spheron = require('spheron');
+var repl = require('repl');
+
+function scare(sphero) {
+	sphero.roll(255, 0);
+
+	setTimeout(function() {
+		sphero.roll(255, 180, 10);
+
+		setTimeout(function() {
+			sphero.roll(0, 0, 10);
+		}, 500);
+	}, 500);
+}
+
+function startSphero(spheroPort, callback) {
+	var sphero = spheron.sphero();
+	sphero.on('open', function(err) {
+		if (err) throw err;
+		console.log('Connected to Sphero...');
+		callback(sphero);
+	});
+
+	sphero.on('error', function(error) {
+		console.log(error);
+	});
+
+	sphero.on('end', function() {
+		console.log('Connection has ended');
+	});
+
+	console.log('Connecting to Sphero...');
+
+	sphero.open(spheroPort);
+}
+
+function startRepl(sphero) {
+	var context = repl.start('Spher⊚n > ').context;
+	context.s = context.sphero = sphero;
+	context.COLORS = spheron.toolbelt.COLORS;
+	context.scare = scare;
+}
+
+startSphero('/dev/cu.Sphero-YBW-RN-SPP', startRepl);
